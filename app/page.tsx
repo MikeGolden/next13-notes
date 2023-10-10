e62f11cb8ab1875f1c29f64229196b3b99685f85
Mike Goldenberg <csrutil@tuta.io>
2023-10-10 23:04:44 +0200

    marking main layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import Image from 'next/image'
-
-export default function HomePage() {
+export default function Home() {
   return (
-      <div>
-          <h1>Home Page</h1>
-          <p>Some content</p>
-      </div>
+    <div>
+      <h1>Welcome to NextSpace!</h1>
+      <p>
+        A next-gen social media app to connect with frens inspired by MySpace
+      </p>
+      <p>To get started, sign up for an account</p>
+    </div>
   );
 }
