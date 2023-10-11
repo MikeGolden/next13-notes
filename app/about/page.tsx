615df6acc3e10c30e9addc819ceb012466def816
Mike Goldenberg <csrutil@tuta.io>
2023-10-11 13:31:48 +0200

    add about page and blog feed

---
+++ app/about/page.tsx
@@ -0,0 +1,17 @@
+import { Metadata } from "next";
+
+export const dynamic = 'force-static'; // not necessary, just for demonstration
+
+export const metadata: Metadata = {
+  title: 'About Us',
+  description: 'About NextSpace',
+};
+
+export default function Blog() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>We are social media company that is just built to be built.</p>
+    </div>
+  );
+}
