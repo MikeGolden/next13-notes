0a49b405268f7cb8c2f237c1e70793c35b956524
Mike Goldenberg <csrutil@tuta.io>
2023-10-13 12:11:10 +0200

    stupid

--- app/api/friends/accept/route.ts
+++ app/api/friends/accept/route.ts
@@ -58,7 +58,7 @@ export async function POST(req: Request) {
         'new_friend',
         friend
       ),
-      db.sadd(`user:${session:user:id}:friends`, idToAdd),
+      db.sadd(`user:${session.user.id}:friends`, idToAdd),
       db.sadd(`user:${idToAdd}:friends`, session.user.id),
       db.srem(`user:${session.user.id}:incoming_friend_requests`, idToAdd),
     ]);
