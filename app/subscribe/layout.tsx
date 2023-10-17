ce8da61bad20b8ed41309d4ea0dd003be0966833
Mike Goldenberg <csrutil@tuta.io>
2023-10-17 13:26:49 +0200

    subscribe UI and api logic

---
+++ app/subscribe/layout.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+ title: "Subscribe",
+};
+
+export default function SubscribeLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <section>{children}</section>;
+}
